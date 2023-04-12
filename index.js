f39b97db51d758fd34253e095e9d3ee7a1972121
ElevenWater975 <nyrangerzepeda21@outlook.com>
2023-04-12 11:56:33 -0700

    Ban appeal page + something in index.html

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 function earlyAccess() {
-    alert("This page is being worked on and it will be released once the site is fully released.")
+    alert("This page is being worked on and it will be released once the site is fully released.");
+}
+
+function soon() {
+    alert("This feature is coming soon");
 }
