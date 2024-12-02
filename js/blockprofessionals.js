9fce390c5d53d50c8df17a7fde389e118b54a8e7
ElevenWater975 <nyrangerzepeda21@outlook.com>
2024-12-01 21:54:34 -0800

    semi mobile compatibility update

---
+++ js/blockprofessionals.js
@@ -0,0 +1,9 @@
+function checkPlaceholder() {
+    const secretone = "storyofyaril";
+    const searchbar = document.getElementById("searchbar");
+    if (searchbar.style.color == "red") {
+        console.log("Hello, World!");
+    } else {
+        console.log("Fuck you");
+    }
+}
