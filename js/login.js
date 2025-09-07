3e3df5b6dd3cc77d3f2024f4b5ae45751b2cb985
ElevenWater975 <nyrangerzepeda21@outlook.com>
2025-09-06 19:51:15 -0700

    new update

---
+++ js/login.js
@@ -0,0 +1,10 @@
+var uname = document.getElementsByName("username");
+var pwd = document.getElementsByName("password");
+
+function selectOne() {
+    uname.innerHTML = "Please enter your username";
+}
+
+function selectTwo() {
+    pwd.innerHTML = "Please enter your password"
+}   
